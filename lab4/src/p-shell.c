24fa6fde15a51b89ffc0e8a7c98185cd1aff1b38
Ethan <ethangbraun@gmail.com>
2023-03-20 18:25:37 -0500

    Cleaned up the code a little

--- lab4/src/p-shell.c
+++ lab4/src/p-shell.c
@@ -15,19 +15,17 @@ void stop (int sig_num) {
 }
 
 
-int main (char *argc[], char *argv[]) {
+int main(char *argc[], char *argv[]) {
+	int i, num, sltime;
 
-
-	int i,num,sltime;
-
-	signal(SIGQUIT,cont);
-	signal(SIGTSTP,stop);
+	signal(SIGQUIT, cont);
+	signal(SIGTSTP, stop);
 	
-	num =atoi(argv[1]);
+	num = atoi(argv[1]);
 	sltime = 1000*atoi(argv[2]);
 	
-	for (i=1; i<=num; i++){
-		printf("This is program %s and it prints for the %d time of %d...\n",argv[0],i,num);
+	for (i = 1; i <= num; i++){
+		printf("This is program %s and it prints for the %d time of %d...\n", argv[0], i, num);
 		usleep(sltime);
 	}
 }
--- lab4/src/shell.c
+++ lab4/src/shell.c
@@ -6,6 +6,7 @@
 #include <wait.h>
 #include <string.h>
 #include <sys/resource.h>
+
 #include "queue.h"
 
 #define FCFS 1
@@ -27,37 +28,22 @@ void help() {
 
 void helpcmd(char *cmd) {
 	printf("This is manual page\n\n");
-	if (strcmp(cmd,"ver")==0)
-	{
+	if (strcmp(cmd, "ver") == 0) {
 		printf("\nver:\tShows details about the shell version\n");
-	}
-	else if (strcmp(cmd,"exec")==0)
-	{
+	} else if (strcmp(cmd, "exec") == 0) {
 		printf("\nexec p1(n1,qt1) p2(n2,qt2) ...:\nExecutes the programs p1, p2 ...\nEach program types a message for n times and it is given a time quantum of qt msec.\n");
 		printf("If parameter (&) is given the program will be executed in the background\n");
-	}
-	else if (strcmp(cmd,"ps")==0)
-	{
+	} else if (strcmp(cmd, "ps") == 0) {
 		printf("\nps:\tShows the living process with the given pid\n");
-	}
-	else if (strcmp(cmd,"kill")==0)
-	{
+	} else if (strcmp(cmd, "kill") == 0) {
 		printf("\nkill pid:\tEnds the process with the given pid\n");
-	}
-	else if (strcmp(cmd,"help")==0)
-	{
+	} else if (strcmp(cmd, "help") == 0) {
 		printf("\nhelp:\tYou should know this command by now\n");
-	}
-	else if (strcmp(cmd,"exit")==0)
-	{
+	} else if (strcmp(cmd, "exit") == 0) {
 		printf("\nexit:\tEnds the experience of working in the new shell\n");
-	}
-	else if (strcmp(cmd,"set_scheduling")==0)
-	{
+	} else if (strcmp(cmd, "set_scheduling") == 0) {
 		printf("\nset_scheduling:\tSets the scheduling policy to one of three values: FCFS, Round Robin, or MFQ\n");
-	}
-	else 
-	{
+	} else {
 		printf("\nNo Such command. Type help to see a list of commands\n");
 	}
 }
@@ -70,15 +56,14 @@ void ps() {
 	struct node *p;
 	printf("\nNEW SHELL presents the following living processes\n");
 	printf("\tPID\tNAME\n");
-	for (p=pid_list.head; p!=NULL; p=p->next)
-	{
+	for (p=pid_list.head; p!=NULL; p=p->next){
 		printf("\t%d\t%s\n",p->pid,p->name);
 	}
 }
 
 void mykill(int pid) {
-	kill(pid,SIGTERM);
-	printf("You have just killed process %d\n",pid );
+	kill(pid, SIGTERM);
+	printf("You have just killed process %d\n", pid);
 }
 
 
@@ -88,30 +73,25 @@ void exec(char *input) {
 	char *temp;
 	struct node *p;
 
-	for (i = 0; i < 10; i++)
-	{
+	for (i = 0; i < 10; i++){
 		args[i]=(char *)malloc(10*sizeof(char));
 	}
 
-	strcpy(args[0],strtok(input,"(,"));
-	for (i=1; (temp=strtok(NULL,",)"))!=NULL; i++) 
+	strcpy(args[0], strtok(input, "(,"));
+	for (i = 1; (temp = strtok(NULL, ",)")) != NULL; i++) 
 		strcpy(args[i],temp);
 	printf("\n");
-	if (strcmp(args[i-1],"&")==0)
-	{
+	if (strcmp(args[i-1], "&") == 0){
 		args[i-1]=NULL;
-	}
-	else
+	} else
 		args[i]=NULL;
-	if ((t=fork())==0)
-	{
-		execv(args[0],args);
+	if ((t = fork()) == 0){
+		execv(args[0], args);
 	}
-	enqueue(t,args[0],&pid_list);
-	if (args[i-1]!=NULL)
-	{
-		fg_pid=t;
-		while(fg_pid!=0 && fg_suspended!=1)
+	enqueue(t,args[0], &pid_list);
+	if (args[i-1]!=NULL){
+		fg_pid = t;
+		while(fg_pid != 0 && fg_suspended != 1)
 			pause();
 	}
 
@@ -120,17 +100,13 @@ void exec(char *input) {
 
 void myexit() {
 	char yesno;
-	if (pid_list.head==pid_list.tail)
-	{
-		run=0;
-	}
-	else {
+	if (pid_list.head == pid_list.tail){
+		run = 0;
+	} else {
 		printf("There are still living processes Do you want to kill them? (y/n): ");
 		yesno=getchar();
-		if (yesno == 'y')
-		{
-			while(pid_list.head!=pid_list.tail)
-			{
+		if (yesno == 'y'){
+			while(pid_list.head != pid_list.tail){
 				mykill(pid_list.tail->pid);
 				usleep(10000);
 			}
@@ -154,31 +130,26 @@ void set_scheduling(char* input){
 void childdead(int signum) {
 	int dead_pid, status;
 	
-	dead_pid=wait(&status);
-	printf("The child %d is dead\n",dead_pid );
+	dead_pid = wait(&status);
+	printf("The child %d is dead\n", dead_pid);
 
 	delete(&pid_list,dead_pid);
 	printf("\n");
-	if (dead_pid==fg_pid)
-	{
+	if (dead_pid == fg_pid){
 		fg_pid=0;
 	}
 }
 
 void susp (int signum) {
-	fg_suspended=1;
+	fg_suspended = 1;
 	printf("All processes supspended\n");
 }
 
 void cont(int signum) {
-	fg_suspended=0;
+	fg_suspended = 0;
 	printf("Waking all processes...\n");
-	while (fg_pid!=0 && fg_suspended!=1)
+	while (fg_pid != 0 && fg_suspended != 1)
 		pause();
-}
-
-void execRoundRobin(char programs[15][30], int num){
-
 }
 
 void execFCFS(char programs[15][30], int num){
@@ -186,11 +157,15 @@ void execFCFS(char programs[15][30], int num){
 		exec(programs[i]);
 }
 
+void execRoundRobin(char programs[15][30], int num){
+
+}
+
 void execMFQ(char programs[15][30], int num){
 
 }
 
-void myExec(char programs[15][30], int num){
+void execAll(char programs[15][30], int num){
 	if(schedPol == FCFS)
 		execFCFS(programs, num);
 	if(schedPol == ROUNDROBIN)
@@ -205,27 +180,27 @@ int main(int argc, char* argv[]) {
 
 	pid_list.head=NULL;
 	pid_list.tail=NULL;
-	enqueue(getppid(),"NEW SHELL",&pid_list);
+	enqueue(getppid(), "NEW SHELL", &pid_list);
 
-	signal(SIGCHLD,childdead);
-	signal(SIGTSTP,susp);
-	signal(SIGQUIT,cont);
-	setpriority(PRIO_PROCESS,0,-20);
+	signal(SIGCHLD, childdead);
+	signal(SIGTSTP, susp);
+	signal(SIGQUIT, cont);
+	setpriority(PRIO_PROCESS, 0, -20);
 
 	ver();
 
 	while (run){
 		printf("=>");
 		for (argnum=0; (scanf("%s",&input[argnum]))==1;argnum++)
-			if (getchar()=='\n') break;
-		if (strcmp(input[0],"ver")==0 && argnum==0) ver();
-		else if (strcmp(input[0],"help")==0 && argnum==0) help();
-		else if (strcmp(input[0],"help")==0 && argnum==1) helpcmd(input[argnum]);
-		else if (strcmp(input[0],"ps")==0 && argnum==0) ps();
-		else if (strcmp(input[0],"kill")==0 && argnum==1) mykill(atoi(input[1]));
-		else if (strcmp(input[0],"exec")==0 && argnum!=0) myExec(input + 1, argnum);
-		else if (strcmp(input[0],"exit")==0 && argnum==0) myexit();
-		else if (strcmp(input[0],"set_scheduling")==0 && argnum>0) set_scheduling(input[1]);
+			if (getchar() == '\n') break;
+		if (strcmp(input[0],"ver") == 0 && argnum==0) ver();
+		else if (strcmp(input[0],"help") == 0 && argnum == 0) help();
+		else if (strcmp(input[0],"help") == 0 && argnum == 1) helpcmd(input[argnum]);
+		else if (strcmp(input[0],"ps") == 0 && argnum == 0) ps();
+		else if (strcmp(input[0],"kill") == 0 && argnum == 1) mykill(atoi(input[1]));
+		else if (strcmp(input[0],"exec") == 0 && argnum != 0) execAll(input + 1, argnum);
+		else if (strcmp(input[0],"exit") == 0 && argnum == 0) myexit();
+		else if (strcmp(input[0],"set_scheduling") == 0 && argnum > 0) set_scheduling(input[1]);
 	    else printf("No such command. Check help for help.\n");
 	}
 
